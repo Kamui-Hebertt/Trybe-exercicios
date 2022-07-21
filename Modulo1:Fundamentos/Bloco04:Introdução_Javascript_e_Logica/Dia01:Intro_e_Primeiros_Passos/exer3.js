0b9325f1457fbd454b9485dbfbc89827f70c438b
Hebertt Nascimento <heberttaferreira.com@gmail.com>
2022-07-20 23:50:20 -0300

    adicionando mais 4 mini programa(exercicio)

---
+++ Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia01:Intro_e_Primeiros_Passos/exer3.js
@@ -0,0 +1,7 @@
+const valor1 = -5;
+
+if (valor1 > 0){console.log("positive")}
+
+else if (valor1 < 0 ){console.log("negative")};
+
+//Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
---
+++ Modulo1:Fundamentos/Bloco04:Introdução_Javascript_e_Logica/Dia01:Intro_e_Primeiros_Passos/exer4.js
@@ -0,0 +1,22 @@
+const valor1 = 60;
+const valor2 = 60;
+var valor3;
+const triangle = 180;
+let istriangle;
+
+if (valor1 || valor2 || valor3 == undefined){console.log("Erro: valor de ângulo não declarado")}
+
+else {
+
+if( valor1 + valor2 + valor3 == triangle)
+
+{istriangle = true
+console.log(istriangle)}
+
+else if (valor1 + valor2 + valor3 !== triangle){
+    istriangle= false
+
+console.log(istriangle)}};
+
+
+//🚀 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
